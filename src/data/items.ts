fcf77869f448ba3ac89cfa9fe71c1d7eab34e915
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-09-20 01:13:59 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import {Item} from '../types/Items';
+
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 20},
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 1200},
+    { date: new Date(2021, 10, 5), category: 'salary', title: 'Salário', value: 3400},
+];
