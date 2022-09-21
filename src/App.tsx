21d9c597a14a7f71482d11580095629467f24521
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-09-21 02:03:10 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import {useState, useEffect} from 'react'
 import * as C from './App.styles'
+import {Item} from './types/Items';
+import {Category} from './types/Category';
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea'
+
 
 const App = () => {
+  const [ list, setList ] = useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([]);
+  const [ currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
@@ -12,7 +28,7 @@ const App = () => {
         
         {/* Área de inserção */}
 
-        {/* Tabela de itens */}
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
--- src/data/items.ts
+++ src/data/items.ts
@@ -2,8 +2,8 @@ import {Item} from '../types/Items';
 
 
 export const items: Item[] = [
-    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
-    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 20},
-    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 1200},
-    { date: new Date(2021, 10, 5), category: 'salary', title: 'Salário', value: 3400},
+    { date: new Date(2022, 8, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2022, 8, 15), category: 'food', title: 'Burger King', value: 20},
+    { date: new Date(2022, 8, 16), category: 'rent', title: 'Aluguel', value: 1200},
+    { date: new Date(2022, 9, 5), category: 'salary', title: 'Salário', value: 3400},
 ];
